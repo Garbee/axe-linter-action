43aeb7f1408f1d7b654fb8e808dc3bd3e2d8a9c8
Jonathan Garbee <jonathan@garbee.me>
2026-02-22 14:17:09 +0000

    convert utils test to native

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,10 +1,13 @@
-import 'mocha'
+import { describe, it } from 'node:test'
 import assert from 'node:assert/strict'
 import { pluralize } from './utils.ts'
 
 describe('pluralize', () => {
-  it('should return the plural form of a word', () => {
+  it('should return the singular form of a word', () => {
     assert.strictEqual(pluralize(1), '')
+  })
+
+  it('should return the plural form of a word', () => {
     assert.strictEqual(pluralize(2), 's')
   })
 })
